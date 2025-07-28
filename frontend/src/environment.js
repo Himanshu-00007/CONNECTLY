429649526c3c65a5b64ee8750c800e8f76d02937
Himanshu Pandey <himanshupandey2003@gmail.com>
2025-07-28 13:34:13 +0530

    environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,3 +1,3 @@
 let IS_PROD=true;
-const server=IS_PROD ? "https://connectly-nm07.onrender.com" : "http://localhost:8000";
+const server=IS_PROD ? "https://connectly-wc1l.onrender.com/" : "http://localhost:8000";
 export default server;
